9d0c25bd814798e8e38029327f7edcebe8ab1e7a
Sebastián Ávila A. <sebastian4j@gmail.com>
2019-05-02 23:20:06 -0400

    version inicial

---
+++ src/test/java/com/sebastian/redis/testcontainers/RedisContainerSetUp.java
@@ -0,0 +1,41 @@
+package com.sebastian.redis.testcontainers;
+
+import com.github.dockerjava.api.command.CreateContainerCmd;
+import com.github.dockerjava.api.model.ExposedPort;
+import com.github.dockerjava.api.model.PortBinding;
+import com.github.dockerjava.api.model.Ports;
+import java.util.function.Consumer;
+import org.testcontainers.containers.GenericContainer;
+
+/**
+ * inicializacion de redis para las pruebas.
+ *
+ * @author Sebastián Ávila A.
+ */
+public final class RedisContainerSetUp {
+
+    /**
+     * inicia redis en un puerto especifico.
+     *
+     * @return
+     */
+    public static GenericContainer puertoEspecifico() {
+        final int containerExposedPort = 6379;
+        final Consumer<CreateContainerCmd> config = e -> e.withPortBindings(new PortBinding(Ports.Binding.bindPort(Integer.
+                parseInt(System.getenv("redis_puerto"))),
+                new ExposedPort(containerExposedPort)));
+        return new GenericContainer("redis:5.0.3-alpine")
+                .withExposedPorts(containerExposedPort)
+                .withCreateContainerCmdModifier(config);
+    }
+
+    /**
+     * inicia redis en un puerto dinamico.
+     *
+     * @return
+     */
+    public static GenericContainer puertoDinamico() {
+        return new GenericContainer<>("redis:5.0.3-alpine")
+                .withExposedPorts(6379);
+    }
+}
---
+++ src/test/java/com/sebastian/redis/testcontainers/RutasRecursosTest.java
@@ -0,0 +1,36 @@
+package com.sebastian.redis.testcontainers;
+
+import java.util.logging.Logger;
+import org.assertj.core.api.Assertions;
+import org.junit.Rule;
+import org.junit.Test;
+import org.testcontainers.containers.GenericContainer;
+import redis.clients.jedis.Jedis;
+
+/**
+ * test utilizando redis con testcontainers.
+ *
+ * @author Sebastian Avila A,
+ *
+ */
+public class RutasRecursosTest {
+
+    private static final Logger LOGGER = Logger.getLogger(RutasRecursosTest.class.getName());
+
+    @Rule
+    public GenericContainer redis = RedisContainerSetUp.puertoDinamico();
+
+    @Test
+    public void redisPuertoDinamicoDisponible() {
+        final var puerto = redis.getFirstMappedPort();
+        Assertions.assertThat(puerto).isGreaterThan(0);
+    }
+
+    @Test
+    public void clientePuedeObtenerConexion() {
+        var cliente = new Jedis(redis.getContainerIpAddress(), redis.getFirstMappedPort());
+        cliente.set("hola", "chao");
+        Assertions.assertThat(cliente.get("hola")).isEqualTo("chao");
+    }
+
+}
